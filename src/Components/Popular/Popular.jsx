cea6e1249a3d409afac3ec62250b35b47e594517
gansharm <91821617+gansharm@users.noreply.github.com>
2024-07-16 19:12:20 +0530

    Update Popular.jsx

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ export const Popular = () => {
  const [popularProducts,setPopularProducts] = useState([])
 
  useEffect(()=>{
-  fetch('http://localhost:4000/popularinwomen')
+  fetch('https://backend-0k29.onrender.com/popularinwomen')
   .then((response)=>response.json())
   .then((data)=>setPopularProducts(data))
  },[])
